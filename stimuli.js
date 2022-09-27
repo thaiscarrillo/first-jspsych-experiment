41c0ea5cb5d72f0ef548c0656a545c9aa32585a5
thaiscarrillo <tcarrill@bowdoin.edu>
2022-09-27 11:48:53 -0400

    experiment with list 1

---
+++ stimuli.js
@@ -0,0 +1,147 @@
+﻿var stimuli = [
+    {
+        "sentence": "You drop a bowling ball on a blackberry",
+        "pic": "blackberryN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a piece of cake",
+        "pic": "cakeS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on an empty can of beer",
+        "pic": "canS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a cracker",
+        "pic": "crackerS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a donut",
+        "pic": "donutS.jpg",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on an egg flan",
+        "pic": "eggflanN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on an egg",
+        "pic": "eggS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a porcelain figure",
+        "pic": "figureS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on glasses",
+        "pic": "glassesN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a hat",
+        "pic": "hatN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a chocolate ice-cream",
+        "pic": "icecreamN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on an iPAD",
+        "pic": "iPadN.jpg",
+        "condition": "Heavy_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on an iPhone",
+        "pic": "iphoneS.png",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a light bulb",
+        "pic": "lampS.jpg",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a mirror",
+        "pic": "mirrorS.jpg",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a chocolate muffin",
+        "pic": "muffinS.jpg",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a paper cup",
+        "pic": "papercupN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a ceramic plate",
+        "pic": "plateN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a strawberry",
+        "pic": "strawberryN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a piece of sushi",
+        "pic": "sushiN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a tile",
+        "pic": "tileN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a tomato",
+        "pic": "tomatoN.jpg",
+        "condition": "Light_Normal",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a balloon on a rice cake",
+        "pic": "tortitaS.jpg",
+        "condition": "Light_Smashed",
+        "List": 1
+    },
+    {
+        "sentence": "You drop a bowling ball on a wineglass",
+        "pic": "wineglassS.jpg",
+        "condition": "Heavy_Smashed",
+        "List": 1
+    }
+]
+
