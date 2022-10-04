bd85a0baaac39699d9accb8bbbd073d245eb9076
thaiscarrillo <tcarrill@bowdoin.edu>
2022-10-04 11:58:13 -0400

    added imagelist

---
+++ practice.js
@@ -0,0 +1,30 @@
+﻿var practicestimuli = [
+    {
+        "sentence": "You drop a bowling ball on a teacup",
+        "pic": "cupB.jpg",
+        "correct": "S",
+        "condition": "practice_s",
+        "List": "practice"
+    },
+    {
+        "sentence": "You drop a balloon on a cigarette",
+        "pic": "cigaretteB.jpg",
+        "correct": "S",
+        "condition": "practice_s",
+        "List": "practice"
+    },
+    {
+        "sentence": "You drop a knife on a hammer",
+        "pic": "hammerB.jpg",
+        "correct": "S",
+        "condition": "practice_s",
+        "List": "practice"
+    },
+    {
+        "sentence": "You drop a fork on a stool",
+        "pic": "tableB.jpg",
+        "correct": "N",
+        "condition": "practice_n",
+        "List": "practice"
+    }
+]
